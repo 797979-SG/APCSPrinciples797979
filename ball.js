b83250bb577ad45b022a53aa7d821c149e238256
797979-SG <54591428+797979-SG@users.noreply.github.com>
2019-09-05 08:19:13 -0700

    Add files via upload

--- ball.js
+++ ball.js
@@ -1,9 +1,11 @@
 class Ball{
-  constructor(x,y,dx,dy){
-    this.x = x;
-    this.y = y;
-    this.dx = dx;
-    this.dy = dy;
+  constructor(x,y,dx,dy,id){
+    this.loc = createVector(x,y);
+    this.vel = createVector(dx,dy);
+    this.acc = createVector(0)
+    this.id = id
+    this.w = 15
+    if(this.id < 0)(this.w = 50)
     this.clr = color(random(255),random(255),random(255))
   }
 
@@ -14,31 +16,47 @@ class Ball{
   }
 
   checkEdges(){
-    if(this.x < 0){
-      this.dx = -this.dx;
+    if(this.loc.x < 0){
+      this.vel.x = -this.vel.x;
       this.clr = color(random(255),random(255),random(255))
     }
-    if(this.x > width){
-      this.dx = -this.dx;
+    if(this.loc.x > width){
+      this.vel.x = -this.vel.x;
       this.clr = color(random(255),random(255),random(255))
     }
-    if(this.y < 0){
-      this.dy = -this.dy;
+    if(this.loc.y < 0){
+      this.vel.y = -this.vel.y;
       this.clr = color(random(255),random(255),random(255))
     }
-    if(this.y > height){
-      this.dy = -this.dy;
+    if(this.loc.y > height){
+      this.vel.y = -this.vel.y;
       this.clr = color(random(255),random(255),random(255))
     }
   }
 
   update(){
-    this.x = this.x + this.dx;
-    this.y = this.y + this.dy;
+    var distToMainBall;
+    if(this.id >= 0){
+      distToMainBall = this.loc.dist(mainBall.loc);
+      if(distToMainBall < 250){
+
+        this.acc = p5.Vector.sub(mainBall.loc, this.loc);
+        this.acc.normalize();
+        this.acc.mult(0.1);
+      }
+      if(distToMainBall < 150){
+        this.acc = p5.Vector.sub(this.loc, mainBall.loc);
+        this.acc.normalize();
+        this.acc.mult(0.5);
+      }
+      this.vel.add(this.acc);
+    }
+    this.loc.add(this.vel);
+    this.vel.limit(5);
 
   }
   render(){
     fill(this.clr);
-    ellipse(this.x, this.y, 11, 11);
+    ellipse(this.loc.x, this.loc.y, this.w, this.w);
   }
-}
+}//  ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++  end Ball
