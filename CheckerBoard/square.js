7f54dc4a7c38d3476e7b6b287cabdabd8e6e8aec
797979-SG <54591428+797979-SG@users.noreply.github.com>
2019-10-07 08:50:55 -0700

    Add files via upload

---
+++ CheckerBoard/square.js
@@ -0,0 +1,20 @@
+class Square{
+  constructor(x , y){
+    this.loc = creatVector(x , y)
+    this.w = 150
+    this.clr = color(random(255),random(255),random(255))
+  }
+}
+function Sqr(){
+//  Add instance variables to your constructor Function
+//What does a square need to know about itself??
+run()
+  render()
+  
+render()
+fill(this.clr)
+rect(this.loc.x, this.loc.y, this.w, this.w)
+
+  }
+
+}//  End of Square constructor function
--- sketch.js
+++ sketch.js
@@ -1,20 +1,36 @@
-//  Your Name
-// 	Date or version number
-//  This is a comment
-//  The setup function function is called once when your program begins
-var list = [3,6,1,8,2,9];
+/*
+**Lab 1007 Ball
+**Checker Board
+**
+*/
+
+
+//  Global variables
+var squares = []  // declare b1 as a global variable
+
+
+// put setup code here
 function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
-  background(5, 5, 5);
-  fill(200, 30, 150);
-  swap(list, 1, 2)
+  background(235);
+  loadSquares();
+  //  traverse array and run each ball
+  for(var i = 0; i < squares.length; i++){
+   squares[i].render();
+  }
 }
-function draw(){
 
+
+function loadSquares(){
+  var rowNum = 0;
+  for(var i = 0; i < 64; i++){
+    var x = 0
+    var y = 1
+
+    var loc = creatVector(x,y):
+  }
 }
-function swap(list, a, b){
-  var temp = list[a]
-  list[a] = list[b]
-  list[b] = temp;
+
+
 }
