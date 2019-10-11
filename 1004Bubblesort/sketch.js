9bef30a64a918ce3210c7fbc69394b8339ca3f2f
797979-SG <54591428+797979-SG@users.noreply.github.com>
2019-10-11 08:50:12 -0700

    bubblesort

---
+++ 1004Bubblesort/sketch.js
@@ -0,0 +1,20 @@
+/*
+**Lab 1007 Ball
+**Checker Board
+**
+*/
+
+
+//  Global variables
+var list = [];  // declare b1 as a global variable
+
+
+// put setup code here
+function setup() {
+  var cnv = createCanvas(800, 800);
+  cnv.position((windowWidth-width)/2, 30);
+  background(235);
+  list = [11, 3, 1, 5, 6, 2, 4]
+function bubbleSort(list){
+  (for i = list.length; i >= 0; i++)
+}
