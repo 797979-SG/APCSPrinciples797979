161f3b763fd16260d69f755d0a41f0839aa93f8e
797979-SG <54591428+797979-SG@users.noreply.github.com>
2019-12-12 08:52:26 -0800

    Add files via upload

---
+++ Lab1212SliderShips/sketch.js
@@ -0,0 +1,36 @@
+//Sebastian Gomez
+var ships = []
+var attraction;
+var sliderAttrac;
+//  The setup function function is called once when your program begins
+function setup() {
+  var cnv = createCanvas(800, 800);
+  cnv.position((windowWidth-width)/2, 30);
+  background(20, 20, 20);
+  fill(200, 30, 150);
+  loadPlanet(10);
+
+  sliderAttrac = createSlider(0.01, 0.5, 0.1)
+  sliderAttrac.position(100,750)
+}
+
+//  The draw function is called @ 30 fps
+function draw() {
+  background(20, 20, 20, 30);
+  runPlanet();
+}
+
+function loadPlanet(n){
+  attraction = new Planet(random(width),random(height),random(-50,50), random(-50,50), -1);
+  for(var i = 0; i < n; i++){
+    ships[i] = new Ships(random(width), random(height), random(-10, 10), random(-10, 10), i);
+  }
+}
+
+function runPlanet(){
+  attraction.run();
+  for(var i = 0; i < ships.length; i++){
+    ships[i].run();
+  }
+
+}
